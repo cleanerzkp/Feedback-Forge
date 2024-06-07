1d9142859b9b225cf581f3332ad57861d22b3e75
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-06-07 13:39:41 +0200

    metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Feedback Forge v0.1",
+  description: "A decentralized feedback platform.",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
