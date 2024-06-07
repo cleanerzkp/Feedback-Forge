0a20ee5eb7118b3dcf3a068b4a3a5935683832d4
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-06-07 13:21:53 +0200

    testinnnng leaderboaard

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon, ChatBubbleBottomCenterIcon, StarIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, ChatBubbleBottomCenterIcon, StarIcon, TrophyIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -33,6 +33,11 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "My Feedback",
     href: "/feedback-me",
     icon: <ChatBubbleBottomCenterIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Leaderboard",
+    href: "/leaderboard",
+    icon: <TrophyIcon className="h-4 w-4" />,
   },
 ];
 
